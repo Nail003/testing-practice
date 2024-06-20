b9b8c300a51d7f4dc85c131264cb8540c4570e12
Nail003 <danialzafarstudy003@gmail.com>
2024-06-20 08:52:40 +0500

    fix: capatalize test cases now include "should" at the start of there error messages

--- capatalize/capatalize.test.js
+++ capatalize/capatalize.test.js
@@ -1,13 +1,13 @@
 import capatalize from "./capatalize";
 
 describe("Capatalization Tests", () => {
-    it(`capatalize the input string`, () => {
+    it(`should capatalize the input string`, () => {
         const input = "Pakistan";
         const output = "PAKISTAN";
         expect(capatalize(input)).toBe(output);
     });
 
-    it(`capatalize the input string`, () => {
+    it(`should capatalize the input string`, () => {
         const input = "Sargodha";
         const output = "SARGODHA";
         expect(capatalize(input)).toBe(output);
