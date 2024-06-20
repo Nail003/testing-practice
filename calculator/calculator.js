b254719d6e2cf2211bbdb5b832c03beacc1ada18
Nail003 <danialzafarstudy003@gmail.com>
2024-06-20 09:20:13 +0500

    feat: add division functionality in calculator object

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,4 +1,8 @@
 export const calculator = {
     add: (a, b) => a + b,
     subtract: (a, b) => a - b,
+    divide: (a, b) => {
+        if (b === 0) return "Error: Division not possible";
+        return a / b;
+    },
 };
--- calculator/calculator.test.js
+++ calculator/calculator.test.js
@@ -28,4 +28,26 @@ describe("Calculator Tests", () => {
             expect(calculator.subtract(a, b)).toBe(-10);
         });
     });
+
+    describe("Division Tests", () => {
+        it("should return division of inputs", () => {
+            const a = 0;
+            const b = 1;
+            expect(calculator.divide(a, b)).toBe(0);
+        });
+
+        it("should return division of inputs", () => {
+            const a = 20;
+            const b = 10;
+            expect(calculator.divide(a, b)).toBe(2);
+        });
+
+        it("should return string that division is not possible", () => {
+            const a = 20;
+            const b = 0;
+            expect(calculator.divide(a, b)).toBe(
+                "Error: Division not possible"
+            );
+        });
+    });
 });
