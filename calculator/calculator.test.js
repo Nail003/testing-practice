a2663aa6d37486ae62b725e02aaae6c2ff837424
Nail003 <danialzafarstudy003@gmail.com>
2024-06-20 09:10:10 +0500

    fix: remove extra "the" in the jest error messages

--- calculator/calculator.test.js
+++ calculator/calculator.test.js
@@ -2,13 +2,13 @@ import { calculator } from "./calculator";
 
 describe("Calculator Tests", () => {
     describe("Addition Tests", () => {
-        it("should return sum of the inputs", () => {
+        it("should return sum of inputs", () => {
             const a = 0;
             const b = 0;
             expect(calculator.add(a, b)).toBe(0);
         });
 
-        it("should return sum of the inputs", () => {
+        it("should return sum of inputs", () => {
             const a = 10;
             const b = 20;
             expect(calculator.add(a, b)).toBe(30);
@@ -16,13 +16,13 @@ describe("Calculator Tests", () => {
     });
 
     describe("Subtraction Tests", () => {
-        it("should return difference of the inputs", () => {
+        it("should return difference of inputs", () => {
             const a = 0;
             const b = 0;
             expect(calculator.subtract(a, b)).toBe(0);
         });
 
-        it("should return difference of the inputs", () => {
+        it("should return difference of inputs", () => {
             const a = 10;
             const b = 20;
             expect(calculator.subtract(a, b)).toBe(-10);
